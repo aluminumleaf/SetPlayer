d48d5f3c958d1e5d83da038d26249e4be48c4d4a
Calvin Loncaric <marvinx03@gmail.com>
2012-02-02 18:08:40 -0800

    Add thresholding

--- Sandbox/hello.py
+++ Sandbox/hello.py
@@ -1,24 +1,34 @@
 
 import cv
+import cv2
+from numpy import *
+#from cv import *
 
 cv.NamedWindow("w1", cv.CV_WINDOW_AUTOSIZE)
 camera_index = 0
+size = 600
 capture = cv.CaptureFromCAM(camera_index)
 
 def repeat():
     global capture #declare as globals since we are assigning to them now
     global camera_index
     frame = cv.QueryFrame(capture)
-    cv.ShowImage("w1", frame)
-    c = cv.WaitKey(10)
-    if(c=="n"): #in "n" key is pressed while the popup window is in focus
+    ratio = float(frame.height) / frame.width
+    print ratio
+    f2 = cv.CreateMat(int(size * ratio), size, cv.CV_8UC3)
+    cv.Resize(frame, f2)
+    cv.Threshold(f2, f2, 100, 255, cv.CV_THRESH_BINARY)
+    cv.ShowImage("w1", f2)
+    c = cv.WaitKey(2)
+    print c
+    if (c == ord('n')): #in "n" key is pressed while the popup window is in focus
         camera_index += 1 #try the next camera index
         capture = cv.CaptureFromCAM(camera_index)
         if not capture: #if the next camera index didn't work, reset to 0.
             camera_index = 0
             capture = cv.CaptureFromCAM(camera_index)
-        elif (c == "q"):
-            sys.exit(0)
+    elif (c == ord('q')):
+        sys.exit(0)
 
 while True:
     repeat()
