ac63f317878bfb4ee3eb243f21ba15f0251dc6da
aluminumleaf <hanpamono@gmail.com>
2012-02-26 00:11:23 -0800

    Added hue-saturation-value split

--- Sandbox/settester.py
+++ Sandbox/settester.py
@@ -1,38 +1,66 @@
 
-from cv2 import *
+from cv import *
 import time
 
 DEFAULT_FILE = "../Pictures/lights_on.png"
 WHITE = 255
 ADAPTIVE_THRESH_BLOCK_SIZE = 51
 
+def displayImage(name, image):
+    ''' Makes a window that displays the given image '''
+    width, height = GetSize(image)
+    #ratio = float(height)/float(width)
+    scale = 0.5
+    newHeight = int(height*scale)
+    newWidth = int(width*scale)
+    scaledImg = CreateMat(newHeight, newWidth, image.type)
+    Resize(image, scaledImg)
+    NamedWindow(name)
+    ShowImage(name, scaledImg)
+
 filename = raw_input('Name of picture is...?\n')
 if filename == "":
     filename = DEFAULT_FILE
 
-#origImg = LoadImage(filename)
-#img = CreateMat(1, 1, CV_8UC1)
-origImg = imread(filename)
-grayImg = cvtColor(origImg, COLOR_RGB2GRAY)
+origImg = LoadImageM(filename)
+displayImage('original', origImg)
 
-#NamedWindow("original")
-#ShowImage("original", img)
-imshow('original', origImg)
+imgSize = GetSize(origImg)
+width, height = imgSize
+img = CreateMat(height, width, CV_8UC1)
+#img = CreateImage(imgSize, 8, 1)
+CvtColor(origImg, img, CV_RGB2GRAY)
+#displayImage('step 1', img)
 
-img = adaptiveThreshold(
-    grayImg, 
-    WHITE, 
-    ADAPTIVE_THRESH_MEAN_C, 
-    THRESH_BINARY, 
-    ADAPTIVE_THRESH_BLOCK_SIZE, 
-    5)
-imshow('adaptive threshold', img)
+AdaptiveThreshold(
+    img, img, 
+    WHITE,
+    CV_ADAPTIVE_THRESH_MEAN_C,
+    CV_THRESH_BINARY,
+    ADAPTIVE_THRESH_BLOCK_SIZE)
+displayImage('step 2', img)
 
-#storage = cv.CreateMemStorage(0)
-#contours = cv.FindContours(img, storage, cv.CV_POLY_APPROX_DP, 3, 1)
+hsvImage = CreateMat(height, width, CV_8UC3)
+#hsvImage = CreateImage(imgSize, 8, 3)
+CvtColor(origImg, hsvImage, CV_RGB2HSV)
 
+hue = CreateMat(height, width, CV_8UC1)
+sat = CreateMat(height, width, CV_8UC1)
+val = CreateMat(height, width, CV_8UC1)
+#hue = CreateImage(imgSize, 8, 1)
+#sat = CreateImage(imgSize, 8, 1)
+#val = CreateImage(imgSize, 8, 1)
+
+Split(hsvImage, hue, sat, val, None)
+
+#displayImage('Hue', hue)
+#displayImage('Saturation', sat)
+#displayImage('Value', val)
+
+#storage = CreateMemStorage(0)
+#contours = FindContours(img, storage, 
 
 while True:
-    # do nothing! :D
+    
     time.sleep(500)
 
