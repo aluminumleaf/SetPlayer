6cea670ff1b15c8fcaea9a7ba57ffb4968cc8309
Calvin Loncaric <marvinx03@gmail.com>
2012-04-06 23:30:15 -0700

    Clean up the code a bit

---
+++ Sandbox/geometry.py
@@ -0,0 +1,152 @@
+
+from math import *
+
+def lineAngle(line):
+    y = line[0][1] - line[1][1]
+    x = line[0][0] - line[1][0]
+    return atan2(y, x)
+
+def angleDiff(a1, a2):
+    a1 = fmod(a1, CV_PI)
+    a2 = fmod(a2, CV_PI)
+    return a2 - a1;
+
+def distance(p1, p2):
+    dx = p1[0] - p2[0]
+    dy = p1[1] - p2[1]
+    return sqrt(dx*dx + dy*dy)
+
+def endpointDistance(line1, line2):
+    return min(distance(line1[0], line2[0]),
+               distance(line1[0], line2[1]),
+               distance(line1[1], line2[0]),
+               distance(line1[1], line2[1]))
+
+def lineVec(line):
+    return ((line[0][0] - line[1][0]), (line[0][1] - line[1][1]))
+
+def vecLen(vec):
+    return distance((0,0),vec)
+
+def dot(v1, v2):
+    ''' Precondition: vectors have nonzero length '''
+    x = float(v1[0] * v2[0])
+    y = float(v1[1] * v2[1])
+    magnitude = vecLen(v1) * vecLen(v2)
+    return (x + y) / magnitude
+
+def intersect(firstLine, secondLine):
+    ''' finds the intersections of two line segments '''
+    # We'll use parametrization here.
+    # The math (since I should show my work):
+    # 
+    # Let the lines be
+    #       first line  = V*s + P
+    #       second line = U*t + Q
+    # where any captial letter A is some vector A = <ax, ay>
+    #
+    # The intersection is where they're equal:
+    #       V*s + P = U*t + Q
+    # which is equivalent to
+    #       vx*s + px = ux*t + qx   (1)
+    #       vy*s + py = uy*t + qy   (2)
+    # 
+    # Solving for s with equation (1) yields
+    #
+    #       s = (ux/vx)*t + (qx - px)/vx
+    #
+    # and plugging that into equation (2) to solve for t yields
+    #
+    #            vx(qy - py) - vy(qx - px)
+    #       t = ---------------------------
+    #                 vy*ux  -  vx*uy
+    #
+    # Plugging t back into the second line's parametric equation
+    # yields our desired point :)
+
+    V = lineVec(firstLine)
+    U = lineVec(secondLine)
+
+    vLen = vecLen(V)
+    uLen = vecLen(U)
+
+    vx = V[0] / vLen
+    vy = V[1] / vLen
+    ux = U[0] / uLen
+    uy = U[1] / uLen
+
+    px = firstLine[0][0]
+    py = firstLine[0][1]
+    qx = secondLine[0][0]
+    qy = secondLine[0][1]
+
+    denominator = (vy*ux  -  vx*uy)
+    if denominator == 0:
+        print "OH NO! NOT DIVIDING BY ZERO!"
+        return (0, 0)
+    numerator = (vx*(qy - py) - vy*(qx - px))
+    t = numerator / denominator
+
+    xCoord = int(ux * t + qx)
+    yCoord = int(uy * t + qy)
+    return (xCoord, yCoord)
+
+def mergeCommonPoint(polyline, line):
+    firstPolyPt = polyline[0]
+    lastPolyPt = polyline[-1]
+    firstLinePt = line[0]
+    lastLinePt = line[-1]
+
+    dist1 = distance(firstPolyPt, firstLinePt)
+    dist2 = distance(firstPolyPt, lastLinePt)
+    dist3 = distance(lastPolyPt, firstLinePt)
+    dist4 = distance(lastPolyPt, lastLinePt)
+    minDist = min(dist1, dist2, dist3, dist4)
+
+    if dist1 == minDist:
+        return [lastLinePt] + polyline
+    elif dist2 == minDist:
+        return [firstLinePt] + polyline
+    elif dist3 == minDist:
+        return polyline + [lastLinePt]
+    else:
+        return polyline + [firstLinePt]
+
+def extractVertices(middle, leg1, leg2):
+    firstIntersect = intersect(middle, leg1)
+    secondIntersect = intersect(middle, leg2)
+    
+    if distance(leg1[0], firstIntersect) < distance(leg1[1], firstIntersect):
+        vertices = [leg1[1], firstIntersect, secondIntersect]
+    else:
+        vertices = [leg1[0], firstIntersect, secondIntersect]
+
+    if distance(leg2[0], secondIntersect) < distance(leg2[1], secondIntersect):
+        vertices += [leg2[1]]
+    else:
+        vertices += [leg2[0]]
+#    vertices = mergeCommonPoint(list(middle), leg1)
+#    vertices = mergeCommonPoint(vertices, leg2)
+    return vertices
+
+def areParallel(firstLine, secondLine):
+    ''' Precondition: firstLine and secondLine have nonzero length '''
+    firstVec = lineVec(firstLine)
+    secondVec = lineVec(secondLine)
+    return abs(dot(firstVec, secondVec)) > 0.95
+
+def areOverlapping(firstLine, secondLine):
+    ''' Checks whether two line segments overlap.
+
+    Assumption: lines are colinear
+    '''
+    a1, a2 = firstLine
+    b1, b2 = secondLine
+
+    if (a1[0] < b1[0]) and (a1[0] < b2[0]) and (a2[0] < b2[0]) and (a2[0] < b2[0]):
+        return False
+
+    if (a1[0] > b1[0]) and (a1[0] > b2[0]) and (a2[0] > b2[0]) and (a2[0] > b2[0]):
+        return False
+
+    return True
--- Sandbox/settester.py
+++ Sandbox/settester.py
@@ -1,9 +1,13 @@
 
+# system imports
 from cv import *
 from math import *
 import random
 import time
 import os
+
+# our imports
+from geometry import *
 
 DEFAULT_FILE = "../Pictures/black_bg.png"
 WHITE = 255
@@ -21,65 +25,47 @@ def displayImage(name, image):
     NamedWindow(name)
     ShowImage(name, scaledImg)
 
-filename = raw_input('Name of picture is...?\n')
-if filename == "":
-    filename = DEFAULT_FILE
+def getImage():
+    filename = raw_input('Name of picture is...?\n')
+    if filename == "":
+        filename = DEFAULT_FILE
+    return LoadImageM(filename)
 
-origImg = LoadImageM(filename)
-displayImage('original', origImg)
+def grayscale(img):
+    if (img.type == CV_8UC1):
+        return img
+    width, height = GetSize(img)
+    result = CreateMat(height, width, CV_8UC1)
+    CvtColor(img, result, CV_RGB2GRAY)
+    return result
 
-imgSize = GetSize(origImg)
-width, height = imgSize
-img = CreateMat(height, width, CV_8UC1)
-CvtColor(origImg, img, CV_RGB2GRAY)
-#displayImage('step 1', img)
+def getHoughLines(img):
+    img = grayscale(img)
 
-AdaptiveThreshold(
-    img, img, 
-    WHITE,
-    CV_ADAPTIVE_THRESH_MEAN_C,
-    CV_THRESH_BINARY,
-    ADAPTIVE_THRESH_BLOCK_SIZE)
-#displayImage('step 2', img)
+    AdaptiveThreshold(
+        img, img, 
+        WHITE,
+        CV_ADAPTIVE_THRESH_MEAN_C,
+        CV_THRESH_BINARY,
+        ADAPTIVE_THRESH_BLOCK_SIZE)
 
-hsvImage = CreateMat(height, width, CV_8UC3)
-#hsvImage = CreateImage(imgSize, 8, 3)
-CvtColor(origImg, hsvImage, CV_RGB2HSV)
+    width, height = GetSize(img)
+    hsvImage = CreateMat(height, width, CV_8UC3)
+    CvtColor(origImg, hsvImage, CV_RGB2HSV)
 
-hue = CreateMat(height, width, CV_8UC1)
-sat = CreateMat(height, width, CV_8UC1)
-val = CreateMat(height, width, CV_8UC1)
+    hue = CreateMat(height, width, CV_8UC1)
+    sat = CreateMat(height, width, CV_8UC1)
+    val = CreateMat(height, width, CV_8UC1)
 
-Split(hsvImage, hue, sat, val, None)
+    Split(hsvImage, hue, sat, val, None)
 
-#displayImage('Hue', hue)
-#displayImage('Saturation', sat)
-#displayImage('Value', val)
+    houghImage = CreateMat(height, width, CV_8UC1)
+    storage = CreateMemStorage(0)
+    Canny(val, houghImage, 230, 250, 3)
 
-#storage = CreateMemStorage(0)
-#contours = FindContours(img, storage, 
+    lines = HoughLines2(houghImage, storage, CV_HOUGH_PROBABILISTIC, 1, CV_PI/180, 50, 10, 20)
 
-houghImage = CreateMat(height, width, CV_8UC1)
-houghImageColor = CreateMat(height, width, CV_8UC3)
-storage = CreateMemStorage(0)
-Canny(val, houghImage, 230, 250, 3)
-
-
-lines = HoughLines2(houghImage, storage, CV_HOUGH_PROBABILISTIC, 1, CV_PI/180, 50, 10, 20)
-
-#print "Got", len(lines), "lines"
-for line in lines:
-    Line(houghImageColor, line[0], line[1], CV_RGB(255, 0, 0), 3, 8)
-
-def lineAngle(line):
-    y = line[0][1] - line[1][1]
-    x = line[0][0] - line[1][0]
-    return atan2(y, x)
-
-def angleDiff(a1, a2):
-    a1 = fmod(a1, CV_PI)
-    a2 = fmod(a2, CV_PI)
-    return a2 - a1;
+    return lines
 
 def randomColor():
     r = random.randint(0, 255)
@@ -87,33 +73,12 @@ def randomColor():
     b = random.randint(0, 255)
     return CV_RGB(r, g, b)
 
-def distance(p1, p2):
-    dx = p1[0] - p2[0]
-    dy = p1[1] - p2[1]
-    return sqrt(dx*dx + dy*dy)
-
-def endpointDistance(line1, line2):
-    return min(distance(line1[0], line2[0]),
-               distance(line1[0], line2[1]),
-               distance(line1[1], line2[0]),
-               distance(line1[1], line2[1]))
-
-def lineVec(line):
-    return ((line[0][0] - line[1][0]), (line[0][1] - line[1][1]))
-
-def vecLen(vec):
-    return distance((0,0),vec)
-
-def dot(v1, v2):
-    ''' Precondition: vectors have nonzero length '''
-    x = float(v1[0] * v2[0])
-    y = float(v1[1] * v2[1])
-    magnitude = vecLen(v1) * vecLen(v2)
-    return (x + y) / magnitude
-
-#print "Finding cards..."
-
 # Match orthogonal lines
+origImg = getImage()
+displayImage('original', origImg)
+width, height = GetSize(origImg)
+houghImageColor = CreateMat(height, width, CV_8UC3)
+lines = getHoughLines(origImg)
 matchedLines = []
 for i in xrange(len(lines)):
     line1 = lines[i]
@@ -140,104 +105,6 @@ for i in xrange(len(lines)):
         color = randomColor()
         Line(houghImageColor, line1[0], line1[1], color, 3, 8)
         Line(houghImageColor, line2[0], line2[1], color, 3, 8)
-        
-displayImage('Hough Lines', houghImageColor)
-
-def mergeCommonPoint(polyline, line):
-    firstPolyPt = polyline[0]
-    lastPolyPt = polyline[-1]
-    firstLinePt = line[0]
-    lastLinePt = line[-1]
-
-    dist1 = distance(firstPolyPt, firstLinePt)
-    dist2 = distance(firstPolyPt, lastLinePt)
-    dist3 = distance(lastPolyPt, firstLinePt)
-    dist4 = distance(lastPolyPt, lastLinePt)
-    minDist = min(dist1, dist2, dist3, dist4)
-
-    if dist1 == minDist:
-        return [lastLinePt] + polyline
-    elif dist2 == minDist:
-        return [firstLinePt] + polyline
-    elif dist3 == minDist:
-        return polyline + [lastLinePt]
-    else:
-        return polyline + [firstLinePt]
-    
-def intersect(firstLine, secondLine):
-    ''' finds the intersections of two line segments '''
-    # We'll use parametrization here.
-    # The math (since I should show my work):
-    # 
-    # Let the lines be
-    #       first line  = V*s + P
-    #       second line = U*t + Q
-    # where any captial letter A is some vector A = <ax, ay>
-    #
-    # The intersection is where they're equal:
-    #       V*s + P = U*t + Q
-    # which is equivalent to
-    #       vx*s + px = ux*t + qx   (1)
-    #       vy*s + py = uy*t + qy   (2)
-    # 
-    # Solving for s with equation (1) yields
-    #
-    #       s = (ux/vx)*t + (qx - px)/vx
-    #
-    # and plugging that into equation (2) to solve for t yields
-    #
-    #            vx(qy - py) - vy(qx - px)
-    #       t = ---------------------------
-    #                 vy*ux  -  vx*uy
-    #
-    # Plugging t back into the second line's parametric equation
-    # yields our desired point :)
-
-    V = lineVec(firstLine)
-    U = lineVec(secondLine)
-
-    vLen = vecLen(V)
-    uLen = vecLen(U)
-
-    vx = V[0] / vLen
-    vy = V[1] / vLen
-    ux = U[0] / uLen
-    uy = U[1] / uLen
-
-    px = firstLine[0][0]
-    py = firstLine[0][1]
-    qx = secondLine[0][0]
-    qy = secondLine[0][1]
-
-    denominator = (vy*ux  -  vx*uy)
-    if denominator == 0:
-        print "OH NO! NOT DIVIDING BY ZERO!"
-        return (0, 0)
-    numerator = (vx*(qy - py) - vy*(qx - px))
-    t = numerator / denominator
-
-    xCoord = int(ux * t + qx)
-    yCoord = int(uy * t + qy)
-    return (xCoord, yCoord)
-
-    
-
-def extractVertices(middle, leg1, leg2):
-    firstIntersect = intersect(middle, leg1)
-    secondIntersect = intersect(middle, leg2)
-    
-    if distance(leg1[0], firstIntersect) < distance(leg1[1], firstIntersect):
-        vertices = [leg1[1], firstIntersect, secondIntersect]
-    else:
-        vertices = [leg1[0], firstIntersect, secondIntersect]
-
-    if distance(leg2[0], secondIntersect) < distance(leg2[1], secondIntersect):
-        vertices += [leg2[1]]
-    else:
-        vertices += [leg2[0]]
-#    vertices = mergeCommonPoint(list(middle), leg1)
-#    vertices = mergeCommonPoint(vertices, leg2)
-    return vertices
 
 cardOutlines = []
 for i in xrange(len(matchedLines)):
@@ -266,28 +133,6 @@ for i in xrange(len(matchedLines)):
             continue
 
         cardOutlines += [(quad, segments)]
-
-def areParallel(firstLine, secondLine):
-    ''' Precondition: firstLine and secondLine have nonzero length '''
-    firstVec = lineVec(firstLine)
-    secondVec = lineVec(secondLine)
-    return abs(dot(firstVec, secondVec)) > 0.95
-
-def areOverlapping(firstLine, secondLine):
-    ''' Checks whether two line segments overlap.
-
-    Assumption: lines are colinear
-    '''
-    a1, a2 = firstLine
-    b1, b2 = secondLine
-
-    if (a1[0] < b1[0]) and (a1[0] < b2[0]) and (a2[0] < b2[0]) and (a2[0] < b2[0]):
-        return False
-
-    if (a1[0] > b1[0]) and (a1[0] > b2[0]) and (a2[0] > b2[0]) and (a2[0] > b2[0]):
-        return False
-
-    return True
 
 def roughlyEqual(v1, v2, threshold):
     return abs(v1 - v2) < threshold
